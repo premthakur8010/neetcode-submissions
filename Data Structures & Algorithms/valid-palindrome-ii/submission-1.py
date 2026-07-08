d8c9498a306ff2497f8cbe82b89bbfcd9e4a1a16
premthakur8010 <160260186+premthakur8010@users.noreply.github.com>
2026-07-08 13:53:57 +0530

    Add: valid-palindrome-ii - submission-1

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-1.py
@@ -0,0 +1,11 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        l = 0
+        r = len(s) - 1
+        while l<r:
+            if s[l] != s[r]:
+                skipl = s[l+1:r+1]
+                skipr  = s[l:r]
+                return skipl == skipl[::-1] or skipr == skipr[::-1]
+            l,r = l+1,r-1
+        return True
